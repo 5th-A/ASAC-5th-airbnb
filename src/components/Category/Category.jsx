8357713ca88cc1dd21a4c73739bea1694e435610
cse <choisui@gmail.com>
2024-05-21 21:53:25 +0900

    #12 design: category bar scroll 구현

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -11,12 +11,14 @@ import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules'
 
 function ButtonComponent({ button }) {
   return (
-    <li style={{ listStyleType: 'none', margin: '0 10px' }}>
+    <li style={{ listStyleType: 'none', height: '90px', margin: '0' }}>
       <div
         style={{
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
+          justifyContent: 'center', // 수직 중앙 정렬 추가
+          height: '100%', // 전체 높이 사용
         }}
       >
         <div
@@ -24,8 +26,27 @@ function ButtonComponent({ button }) {
           className='w-10 h-[49px] flex-col justify-start items-center gap-2 inline-flex'
         >
           <div className='w-6 h-6 justify-center items-center inline-flex' />
-          <div className='flex-col justify-start items-start gap-4 flex'>
-            <div className="text-neutral-500 text-sm font-['SF Pro']">{button.text}</div>
+          <div
+            className='flex-col justify-start items-start gap-4 flex'
+            style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+          >
+            <img
+              style={{
+                width: '24px',
+                height: '24px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              src={button.icon}
+              alt={button.text}
+            />
+            <div
+              style={{ width: '90px', height: '16px', textAlign: 'center' }}
+              className="text-neutral-500 text-sm font-['SF Pro']"
+            >
+              {button.text}
+            </div>
           </div>
         </div>
       </div>
@@ -37,8 +58,8 @@ export default function Category() {
   const [buttonInfo, setButtonInfo] = useState(example)
 
   return (
-    <div style={{ width: '100%', height: '90px', padding: '0 80px', border: '2px solid red' }}>
-      <div style={{ display: 'flex', height: '78px' }}>
+    <div style={{ width: '100%', height: '90px', padding: '0 80px' }}>
+      <div style={{ display: 'flex', height: '90px' }}>
         <Swiper
           cssMode={true}
           navigation={true}
@@ -49,16 +70,17 @@ export default function Category() {
           spaceBetween={10}
           modules={[Navigation, Mousewheel, Keyboard]}
           className='mySwiper'
+          autoHeight={true}
+          style={{ height: '90px' }} // Swiper의 height를 부모 div와 일치시키기
         >
-          {buttonInfo.map((button, idx) => (
-            <SwiperSlide key={idx}>
-              <ButtonComponent button={button} />
-            </SwiperSlide>
-          ))}
+          <ul style={{ display: 'flex', padding: '0', margin: '0' }}>
+            {buttonInfo.map((button, idx) => (
+              <SwiperSlide key={idx}>
+                <ButtonComponent button={button} />
+              </SwiperSlide>
+            ))}
+          </ul>
         </Swiper>
-        <div>
-          <button>filter</button>
-        </div>
       </div>
     </div>
   )
