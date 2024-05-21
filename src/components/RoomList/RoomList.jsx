92e619733d72753c9bd8bfabc0955ceec4946b52
parby8605 <164837908+parby8605@users.noreply.github.com>
2024-05-21 17:28:08 +0900

    #5 style: commit용 조정

--- src/components/RoomList/RoomList.jsx
+++ src/components/RoomList/RoomList.jsx
@@ -1,7 +1,7 @@
 import RoomCard from './RoomCard'
 
 export default function RoomList() {
-  const roomCount = 20
+  const roomCount = 25
   const roomCards = []
   for (let i = 0; i < roomCount; i++) {
     roomCards.push(<RoomCard key={i} />)
@@ -10,7 +10,7 @@ export default function RoomList() {
     <>
       <div className='flex justify-center pt-5 px-20'>
         <div className='box-border justify-center grid grid-cols-5 grid-rows-3 w-9/10 gap-3 auto-rows-fr'>
-          {/* 임시로 때려박은 RoomCard 20개 */}
+          {/* 임시로 때려박은 RoomCard 25개 */}
           {roomCards}
         </div>
       </div>
