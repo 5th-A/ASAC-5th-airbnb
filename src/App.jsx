a4d589f3ce39efc9726c96dfcad17b526a270c36
kukwon Jeong <rnrdnjs415@daum.net>
2024-05-21 18:02:08 +0900

    #11 design : 최상단바 네이게이션바 UI구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
+import Container from './components/Container/Container.jsx'
 import RoomList from './components/RoomList/RoomList.jsx'
 import SearchBar from './components/SearchBar/SearchBar'
 import Footer from './components/Footer/Footer.jsx'
 function App() {
   return (
     <>
+      <Container />
       <SearchBar />
       <RoomList></RoomList>
       <Footer />
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,88 @@
+function Container() {
+  return (
+    <>
+      <container className="flex justify-between items-center top-0 h-[80px] fixed w-screen px-10 ">
+        <div className="w-[370px]">
+          <a href="App.jsx">
+            <img
+              className="w-[102px] h-[32px]"
+              src="https://s3-alpha-sig.figma.com/img/3764/0204/d9db35f46d0e258b7b07ac83a5018eeb?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XQy4l4qeoruZeTpU3MdXrQhffLAA8AoSw0uIezblCuJNzdz8g-0c8lL1Sfgq2ujFQshZbcrhcZEJVvgiQZ9t-AiQiFOq-1rGyzQo5ck53CMU5vXAWBlqRdmudGmI2APSXdLowm9PV5mL9arUlK1FzVS3vFInNwKtaZ-hdlLrOKIvWIn1XsV9pZX7NrR14aYaA2Mi8o09QPXe75MvomuIljNdie4TTXu~UdSD16wj-xXRdy16pECtB7yCyE9axoSxCv4HmAvHIdEUiCxOP1b6ZYXiayZznO9PUD3Iq2~6LnazpHagiCC6MCVZ5nMVJpnp3ZDYvCzIGI6h0BrhVMF6Dg__ "
+            />
+          </a>
+        </div>
+        <div className="w-[300px] justify-start items-center gap-6 flex px-3.5">
+          <button className="bg-white border-none outline-none focus:font-black ">
+            숙소
+          </button>
+          <button className="bg-white  border-none focus:font-black">
+            체험
+          </button>
+          <button className="bg-white  border-none focus:font-black">
+            온라인체험
+          </button>
+        </div>
+        <div>
+          <div className="Login w-[360px] h-[38px] justify-center items-center gap-6 inline-flex">
+            <div className="BecomeAHost text-neutral-800 text-sm font-black ">
+              당신의 공간을 에어비앤비하세요
+            </div>
+            <div className="Globe Frame20 w-4 h-4 relative flex-col justify-start items-start inline-flex">
+              <div>
+                <svg
+                  width="16"
+                  height="16"
+                  fill="currentcolor"
+                  display="block"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  aria-hidden="true"
+                  role="presentation"
+                  focusable="false"
+                >
+                  <path d="M8 .25a7.77 7.77 0 0 1 7.75 7.78 7.75 7.75 0 0 1-7.52 7.72h-.25A7.75 7.75 0 0 1 .25 8.24v-.25A7.75 7.75 0 0 1 8 .25zm1.95 8.5h-3.9c.15 2.9 1.17 5.34 1.88 5.5H8c.68 0 1.72-2.37 1.93-5.23zm4.26 0h-2.76c-.09 1.96-.53 3.78-1.18 5.08A6.26 6.26 0 0 0 14.17 9zm-9.67 0H1.8a6.26 6.26 0 0 0 3.94 5.08 12.59 12.59 0 0 1-1.16-4.7l-.03-.38zm1.2-6.58-.12.05a6.26 6.26 0 0 0-3.83 5.03h2.75c.09-1.83.48-3.54 1.06-4.81zm2.25-.42c-.7 0-1.78 2.51-1.94 5.5h3.9c-.15-2.9-1.18-5.34-1.89-5.5h-.07zm2.28.43.03.05a12.95 12.95 0 0 1 1.15 5.02h2.75a6.28 6.28 0 0 0-3.93-5.07z"></path>
+                </svg>
+              </div>
+            </div>
+
+            <button className="Frame w-[86px] h-[48px] border  rounded-[29px] border-gray-200 py-2 pr-2 pl-3.5 inline-flex justify-center items-center gap-3 ">
+              <svg
+                display="block"
+                fill="none"
+                height="16px"
+                width="16px"
+                stroke="currentcolor"
+                strokeWidth="3"
+                overflow="visible"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+                aria-hidden="true"
+                role="presentation"
+                focusable="false"
+              >
+                <g fill="none">
+                  <path d="M2 16h28M2 24h28M2 8h28"></path>
+                </g>
+              </svg>
+
+              <svg
+                display="block"
+                height="100%"
+                width="100%"
+                fill="gray"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+                aria-hidden="true"
+                role="presentation"
+                focusable="false"
+              >
+                <path d="M16 .7C7.56.7.7 7.56.7 16S7.56 31.3 16 31.3 31.3 24.44 31.3 16 24.44.7 16 .7zm0 28c-4.02 0-7.6-1.88-9.93-4.81a12.43 12.43 0 0 1 6.45-4.4A6.5 6.5 0 0 1 9.5 14a6.5 6.5 0 0 1 13 0 6.51 6.51 0 0 1-3.02 5.5 12.42 12.42 0 0 1 6.45 4.4A12.67 12.67 0 0 1 16 28.7z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </container>
+    </>
+  );
+}
+
+export default Container;
