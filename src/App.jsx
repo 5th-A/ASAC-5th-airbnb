b48c0aebceae2b410cbab4414d98bdbc174793e8
DOEKYONG <apochmn@naver.com>
2024-05-21 21:53:20 +0900

    #15 refactor: whitespace-nowrap css 추가, 버튼 컴포넌트 이동
    - 화면을 줄였을 때 글자들이 영역 밖을 벗어나지 않게 하기 위해 whitespace-nowrap css 추가
    - 큰 서치바의  버튼을 재사용 관점에서 봤을 때 재사용 가능성이 없어 버튼 컴포넌트를 서치바 컴포넌트로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Container from './components/Container/Container.jsx'
 import RoomList from './components/RoomList/RoomList.jsx'
 import SearchBar from './components/SearchBar/SearchBar'
 import Footer from './components/Footer/Footer.jsx'
+import SmallSearchBar from './components/SearchBar/SmallSearchBar.jsx'
 function App() {
   return (
     <>
-      <Container />
+      {/* <Container /> */}
       <SearchBar />
-      <RoomList></RoomList>
-      <Footer />
+      {/* <SmallSearchBar></SmallSearchBar> */}
+      {/* <RoomList></RoomList>
+      <Footer /> */}
     </>
   )
 }
