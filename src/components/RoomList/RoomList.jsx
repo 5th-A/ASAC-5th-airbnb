3c7d399575af1e0f364ddd24537aed64c64aeeb1
parby8605 <164837908+parby8605@users.noreply.github.com>
2024-05-21 15:41:59 +0900

    #5 feat: grid box로 리스트 구현

--- src/components/RoomList/RoomList.jsx
+++ src/components/RoomList/RoomList.jsx
@@ -1,9 +1,19 @@
 import RoomCard from './RoomCard'
 
 export default function RoomList() {
+  const roomCount = 20
+  const roomCards = []
+  for (let i = 0; i < roomCount; i++) {
+    roomCards.push(<RoomCard key={i} />)
+  }
   return (
     <>
-      <RoomCard></RoomCard>
+      <div className='flex justify-center'>
+        <div className='justify-center  px-20 grid grid-cols-4 box-border w-9/10 gap-2 grid-auto-rows-100px'>
+          {/* 임시로 때려박은 RoomCard 20개 */}
+          {roomCards}
+        </div>
+      </div>
     </>
   )
 }
