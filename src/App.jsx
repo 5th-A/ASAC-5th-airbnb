67ae07741cb5cc5384bec682c5376224a6d1ff1a
parby8605 <164837908+parby8605@users.noreply.github.com>
2024-05-21 11:27:13 +0900

    #3 feat: RoomCard구현
    -상단 추가 정보 및 Like버튼 추가 예정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
+import RoomCard from './components/RoomCard'
 
 function App() {
-
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <RoomCard></RoomCard>
     </>
   )
 }
--- src/components/RoomCard.jsx
+++ src/components/RoomCard.jsx
@@ -1,11 +1,59 @@
 import { useState, useRef, useEffect } from 'react'
-import './RoomCard.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+
 import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+import './RoomCard.css'
 
 export default function RoomCard() {
   return (
     <>
-      <div className='roomWrapper'></div>
+      {/*Image slider*/}
+      <div className='max-w-sm overflow-hidden shadow-lg '>
+        <div className='relative px-2 py-2'>
+          <Swiper
+            className='swiper'
+            modules={[Pagination, Navigation]}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            }}
+            spaceBetween={50}
+            slidesPerView={1}
+            pagination
+          >
+            {/*더미데이터 생기면 map으로 불러오기*/}
+            <SwiperSlide>
+              <img className='rounded-2xl' src='http://placehold.it/110x145' alt='Room view 1' />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='rounded-2xl' src='http://placehold.it/110x145' alt='Room view 2' />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className='rounded-2xl' src='http://placehold.it/110x145' alt='Room view 3' />
+            </SwiperSlide>
+            <div className='swiper-button-prev'></div>
+            <div className='swiper-button-next'></div>
+          </Swiper>
+        </div>
+        {/*Room Info*/}
+        <div className='px-6 py-4'>
+          <div className='flex justify-between items-center'>
+            <div className='inline-block font-bold text-xl mb-1'>한국 어딘가</div>
+            <div className='inline-block text-sm font-semibold text-gray-700'>★ 5.00</div>
+          </div>
+          <div className='text-gray-700 text-base'>겁나 멀어요</div>
+          <div className='text-gray-700 text-base'>여기는 기간</div>
+        </div>
+
+        {/*Room cost*/}
+        <div className='px-5 pt-4 pb-2 flex justify-between items-center'>
+          <div className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>₩999,999/박</div>
+        </div>
+      </div>
     </>
   )
 }
