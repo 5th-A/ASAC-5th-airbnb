c20e77311994c7793cfdbf30c54b3faa9c5f2ad0
parby8605 <164837908+parby8605@users.noreply.github.com>
2024-05-21 09:28:45 +0900

    #3 feat: 숙소 카드 구현
    - 숙소 카드 컴포넌트를 위한 JSX 파일 추가
    - 스타일링을 위한 CSS 파일 추가

---
+++ src/components/RoomCard.jsx
@@ -0,0 +1,10 @@
+import { useState, useRef, useEffect } from 'react'
+import './RoomCard.css'
+
+export default function RoomCard() {
+  return (
+    <>
+      <div className='roomWrapper'></div>
+    </>
+  )
+}
