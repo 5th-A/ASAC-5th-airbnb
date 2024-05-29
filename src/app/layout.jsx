1c4535a31c126aaee60172b7d3a64f94bb193cfb
DOEKYONG <apochmn@naver.com>
2024-05-29 10:54:45 +0900

    #35 [FE] refactor : 헤더 수정 및 rename

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,19 @@
 import './index.css'
+import Footer from '@/components/Footer/Footer'
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="UTF-8" />
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='UTF-8' />
+
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>React App</title>
-        <meta name="description" content="Web site created..." />
+        <meta name='description' content='Web site created...' />
       </head>
       <body>
-        <div id="root">{children}</div>
+        <div id='root'>{children}</div>
+        <Footer />
       </body>
     </html>
   )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
-import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
-      <div>Hello, World!</div>
-
-      <Footer />
+      <Header />
     </main>
   )
 }
