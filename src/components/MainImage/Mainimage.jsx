5dd816eaae511053723d8a762b5e5114f26fbf17
cse <choisui@gmail.com>
2024-05-30 19:28:33 +0900

    #cse feat: 이미지 로직 수정-map 함수 사용

--- src/components/MainImage/Mainimage.jsx
+++ src/components/MainImage/Mainimage.jsx
@@ -3,71 +3,49 @@ import roomDetail from '@/data/roomDetail.json'
 import shareIcon from '/public/assets/share.svg'
 import heartIcon from '/public/assets/heart.svg'
 
+function ButtonComponent({ buttonSrc, text }) {
+  return (
+    <button className='button flex items-center space-x-1'>
+      <img src={buttonSrc} alt={text + ' 아이콘'} className='h-4 w-4' />
+      <span className='text-sm'>{text}</span>
+    </button>
+  )
+}
+
+function ButtonGroup() {
+  return (
+    <div className='flex space-x-2 button-container w-[140px] h-[28px]'>
+      <ButtonComponent buttonSrc={shareIcon} text={'공유하기'} />
+      <ButtonComponent buttonSrc={heartIcon} text={'저장하기'} />
+    </div>
+  )
+}
+
 const MainImage = () => {
   const room = roomDetail[0]
 
   return (
-    <div className='container mx-auto flex flex-col items-center'>
-      <div className='flex justify-between items-center w-full max-w-[1120px]'>
+    <div className='container mx-auto pt-12'>
+      <div className='flex justify-between items-center'>
         <h1 className='text-2xl font-bold'>{room.roomName}</h1>
-        <div className='flex space-x-2 button-container' style={{ width: '140px', height: '28px' }}>
-          <button className='button flex items-center space-x-1'>
-            <img src={shareIcon.src} alt='공유하기 아이콘' className='h-4 w-4' />
-            <span className='text-sm'>공유하기</span>
-          </button>
-          <button className='button flex items-center space-x-1'>
-            <img src={heartIcon.src} alt='저장 아이콘' className='h-4 w-4' />
-            <span className='text-sm'>저장</span>
-          </button>
-        </div>
+        <ButtonGroup />
       </div>
-      <div className='w-full max-w-[1120px] pt-6'>
-        <div className='grid grid-cols-4 grid-rows-2 gap-4 h-[324px]'>
-          <div className='col-span-4 lg:col-span-2 row-span-1 lg:row-span-2 image-container'>
-            <button className='w-full h-full p-0 border-none'>
-              <img
-                src={room.RoomImages[0].url}
-                alt='Building'
-                className='w-full h-full object-cover rounded'
-              />
-            </button>
-          </div>
-          <div className='col-span-1 lg:col-span-1 row-span-1 lg:row-span-1 image-container'>
-            <button className='w-full h-full p-0 border-none'>
-              <img
-                src={room.RoomImages[1].url}
-                alt='Room 1'
-                className='w-full h-full object-cover rounded'
-              />
-            </button>
-          </div>
-          <div className='col-span-1 lg:col-span-1 row-span-1 lg:row-span-1 image-container'>
-            <button className='w-full h-full p-0 border-none'>
-              <img
-                src={room.RoomImages[2].url}
-                alt='Room 2'
-                className='w-full h-full object-cover rounded'
-              />
-            </button>
-          </div>
-          <div className='col-span-1 lg:col-span-1 row-span-1 lg:row-span-1 image-container'>
-            <button className='w-full h-full p-0 border-none'>
-              <img
-                src={room.RoomImages[3].url}
-                alt='Room 3'
-                className='w-full h-full object-cover rounded'
-              />
-            </button>
-          </div>
-          <div className='col-span-1 lg:col-span-1 row-span-1 lg:row-span-1 image-container'>
-            <button className='w-full h-full p-0 border-none'>
-              <img
-                src={room.RoomImages[4].url}
-                alt='Room 4'
-                className='w-full h-full object-cover rounded'
-              />
-            </button>
-          </div>
+      <div style={{ width: '1120px', height: '324px' }} className='w-full pt-6'>
+        <div className='grid grid-cols-4 grid-rows-2 gap-4 h-full'>
+          {room.RoomImages.map((image, index) => (
+            <div
+              key={image.id}
+              className={`col-span-1 lg:col-span-${index === 0 ? '2' : '1'} row-span-1 lg:row-span-${index === 0 ? '2' : '1'} image-container`}
+            >
+              <button className='w-full h-full p-0 border-none'>
+                <img
+                  src={image.url}
+                  alt={`Room ${index}`}
+                  className='w-full h-full object-cover rounded'
+                />
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
