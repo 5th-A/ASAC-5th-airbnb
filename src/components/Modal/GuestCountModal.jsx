b6e91f714093ddd24fd232c6a35002eb09f3c54e
parby8605 <164837908+parby8605@users.noreply.github.com>
2024-05-31 01:57:07 +0900

    #39 desing: 게스트설정 모달 레이아웃 조정

--- src/components/Modal/GuestCountModal.jsx
+++ src/components/Modal/GuestCountModal.jsx
@@ -1,28 +1,45 @@
 'use client'
 import { useState } from 'react'
 
+function getDetailInfo(type) {
+  switch (type) {
+    case '성인':
+      return '13세 이상'
+    case '청소년':
+      return '2~12세'
+    case '아동':
+      return '2세 미만'
+    case '반려동물':
+      return '보조동물을 동반하시나요?'
+    default:
+      return ''
+  }
+}
+
+function CounterButton({ onClick, children }) {
+  return (
+    <button
+      className='border border-solid border-[rgb(176,176,176)] hover:border-black rounded-full text-[22px] text-[rgb(176,176,176)] hover:text-black w-8 h-8 flex items-center justify-center'
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
 function GuestCountButton({ type, setType, children }) {
   return (
-    <div className='flex flex-row justify-between p-4'>
-      <div>{children}</div>
-      <div className='flex flex-row w-[35%] justify-between'>
-        <button
-          onClick={() => {
-            setType((prev) => {
-              return prev + 1
-            })
-          }}
-        >
-          +
-        </button>
-        <div>{type}</div>
-        <button
-          onClick={() => {
-            setType((prev) => (prev !== 0 ? prev - 1 : prev))
-          }}
-        >
+    <div className='flex flex-row justify-between py-4 items-center'>
+      <div>
+        <div className='text-[16px] font-semibold pb-1'>{children}</div>
+        <div className='text-[14px]'>{getDetailInfo(children)}</div>
+      </div>
+      <div className='flex flex-row justify-between items-center'>
+        <CounterButton onClick={() => setType((prev) => prev + 1)}>+</CounterButton>
+        <div className='px-2'>{type}</div>
+        <CounterButton onClick={() => setType((prev) => (prev !== 0 ? prev - 1 : prev))}>
           -
-        </button>
+        </CounterButton>
       </div>
     </div>
   )
@@ -33,6 +50,7 @@ export default function GuestCountModal() {
   const [teens, setTeens] = useState(0)
   const [kids, setKids] = useState(0)
   const [pets, setPets] = useState(0)
+
   return (
     <div className='absolute z-50 bg-white w-full border border-solid rounded-xl border-gray-300'>
       <div className='p-4 mb-4'>
@@ -49,7 +67,6 @@ export default function GuestCountModal() {
           반려동물
         </GuestCountButton>
       </div>
-      <div></div>
     </div>
   )
 }
