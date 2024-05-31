db8031161cd9824a2535aa0b686073cf3e536af9
cse <choisui@gmail.com>
2024-05-31 11:16:23 +0900

    #20 fix: 상세 페이지 이미지 레이아웃 버그 수정 & img 태그 -> Image 컴포넌트로 교체

--- src/components/MainImage/Mainimage.jsx
+++ src/components/MainImage/Mainimage.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 import roomDetail from '@/data/roomDetail.json'
-import shareIcon from '/public/assets/share.svg'
-import heartIcon from '/public/assets/heart.svg'
 
 function ButtonComponent({ buttonSrc, text }) {
   return (
-    <button className='flex items-center space-x-1 p-2'>
-      <img src={buttonSrc} alt={text + ' 아이콘'} className='h-4 w-4' />
+    <button className='button flex items-center space-x-1'>
+      <Image src={buttonSrc} alt={text + ' 아이콘'} width={16} height={16} />
       <span className='text-sm'>{text}</span>
     </button>
   )
@@ -14,9 +13,9 @@ function ButtonComponent({ buttonSrc, text }) {
 
 function ButtonGroup() {
   return (
-    <div className='flex space-x-2 button-container'>
-      <ButtonComponent buttonSrc={shareIcon.src} text={'공유하기'} />
-      <ButtonComponent buttonSrc={heartIcon.src} text={'저장하기'} />
+    <div className='flex space-x-2 button-container w-[140px] h-[28px]'>
+      <ButtonComponent buttonSrc='/assets/share.svg' text={'공유하기'} />
+      <ButtonComponent buttonSrc='/assets/heart.svg' text={'저장하기'} />
     </div>
   )
 }
@@ -25,23 +24,29 @@ const MainImage = () => {
   const room = roomDetail[0]
 
   return (
-    <div className='container mx-auto flex flex-col items-center'>
-      <div className='flex justify-between items-center w-full max-w-[1120px]'>
+    <div className='container mx-auto pt-12'>
+      <div className='flex justify-between items-center'>
         <h1 className='text-2xl font-bold'>{room.roomName}</h1>
         <ButtonGroup />
       </div>
-      <div style={{ width: '1120px', height: '324px' }} className='w-full pt-6'>
+      <div className='w-full pt-6' style={{ width: '1120px', height: '324px' }}>
         <div className='grid grid-cols-4 grid-rows-2 gap-4 h-full'>
           {room.RoomImages.map((image, index) => (
             <div
               key={image.id}
-              className={`col-span-1 lg:col-span-${index === 0 ? '2' : '1'} row-span-1 lg:row-span-${index === 0 ? '2' : '1'} image-container`}
+              className={`relative ${
+                index === 0
+                  ? 'col-span-4 lg:col-span-2 row-span-2'
+                  : 'col-span-1 lg:col-span-1 row-span-1'
+              } image-container`}
             >
-              <button className='w-full h-full p-0 border-none'>
-                <img
+              <button className='w-full h-full p-0 border-none relative'>
+                <Image
                   src={image.url}
                   alt={`Room ${index}`}
-                  className='w-full h-full object-cover rounded'
+                  layout='fill'
+                  objectFit='cover'
+                  className='rounded'
                 />
               </button>
             </div>
