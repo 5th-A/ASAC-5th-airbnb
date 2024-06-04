e980b20956f67dcad7fdb67843431682c228614e
DOEKYONG <apochmn@naver.com>
2024-06-04 10:30:34 +0900

    #40 feat: 체크인 체크아웃 부분 기능 구현 완료

---
+++ src/components/SearchBar/MainSearchBar/DateArea.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const DateArea = ({
+  onClickCheckin,
+  onClickCheckout,
+  isActiveCheckin,
+  isActiveCheckout,
+  selectedStartDate,
+  selectedEndDate,
+}) => {
+  return (
+    <>
+      <div className='flex w-2/6'>
+        <div
+          className={`Frame21 w-1/2 h-[63px] px-6 py-3.5 flex justify-between items-center gap-1 inline-flex hover:bg-gray-200 hover:rounded-full cursor-pointer ${isActiveCheckin ? 'bg-white rounded-full' : 'bg-transparent'}`}
+          onClick={onClickCheckin}
+        >
+          <div>
+            <div className="CheckIn text-neutral-800 text-xs font-['SF Pro']">체크인</div>
+            <div className="AddDates text-neutral-500 text-sm font-normal font-['SF Pro']">
+              {selectedStartDate ? new Date(selectedStartDate).toLocaleDateString() : '날짜 추가'}
+            </div>
+          </div>
+          <div
+            className={`Line11 h-[32px] w-0 border border-neutral-300 border-solid group-hover:border-transparent ${isActiveCheckin ? 'border-transparent' : ''}`}
+          ></div>
+        </div>
+        <div
+          className={`Frame21 w-1/2 h-[63px] px-6 py-3.5 flex justify-between items-center gap-1 inline-flex hover:bg-gray-200 hover:rounded-full cursor-pointer ${isActiveCheckout ? 'bg-white rounded-full' : 'bg-transparent'}`}
+          onClick={onClickCheckout}
+        >
+          <div>
+            <div className="CheckOut text-neutral-800 text-xs font-['SF Pro']">체크아웃</div>
+            <div className="AddDates text-neutral-500 text-sm font-normal font-['SF Pro']">
+              {selectedEndDate ? new Date(selectedEndDate).toLocaleDateString() : '날짜 추가'}
+            </div>
+          </div>
+          <div
+            className={`Line11 h-[32px] w-0 border border-neutral-300 border-solid group-hover:border-transparent ${isActiveCheckout ? 'border-transparent' : ''}`}
+          ></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default DateArea
