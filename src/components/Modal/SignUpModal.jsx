6385c188a416769f576935e8fd8138596cde93df
cse <choisui@gmail.com>
2024-06-04 13:51:43 +0900

    #48 design: 모달 레이아웃 조정

--- src/components/Modal/SignUpModal.jsx
+++ src/components/Modal/SignUpModal.jsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react'
 const SignUpModal = ({ isShow, closeModal }) => {
   useEffect(() => {
     function handleOutsideClick(e) {
-      // modal-content를 클릭한 경우가 아니라면 모달을 닫습니다.
       if (!e.target.closest('#modal-content')) {
         closeModal()
       }
@@ -19,19 +18,34 @@ const SignUpModal = ({ isShow, closeModal }) => {
   if (!isShow) return null
 
   return (
-    <div
-      className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50'
-      id='modal-backdrop'
-    >
-      <div className='p-5 bg-white rounded-lg flex flex-col items-center' id='modal-content'>
-        <ul className='list-none p-0'>
-          <li className='mb-2 cursor-pointer'>회원가입</li>
-          <li className='mb-2 cursor-pointer'>로그인</li>
-          <li className='mb-2 cursor-pointer'>당신의 공간을 에어비앤비하세요</li>
-          <li className='mb-2 cursor-pointer'>도움말 센터</li>
-        </ul>
+    <>
+      <div
+        className='rounded-lg text-sm fixed top-20 right-10 flex justify-center items-center shadow-lg shadow-gray-300 bg-opacity-50 z-50 '
+        id='modal-backdrop'
+      >
+        <div
+          className=' text-left bg-white rounded-3xl flex flex-col items-center shadow-gray-100 '
+          id='modal-content'
+        >
+          <ul className='list-none'>
+            <button className='block'>
+              <li className='my-1 px-2 py-2 cursor-pointer hover:bg-gray-100'>회원가입</li>
+            </button>
+            <button className='block'>
+              <li className='block my-1 px-2 py-2 cursor-pointer hover:bg-gray-100'>로그인</li>
+            </button>
+            <button className='block'>
+              <li className='my-1 px-2 py-2 cursor-pointer hover:bg-gray-100'>
+                당신의 공간을 에어비앤비하세요
+              </li>
+            </button>
+            <button className='block'>
+              <li className='my-1 px-2 py-2 cursor-pointer hover:bg-gray-100'>도움말 센터</li>
+            </button>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
