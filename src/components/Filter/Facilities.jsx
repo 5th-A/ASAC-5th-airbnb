9251d07b40d57c0d0556d0553f02379ed665521d
kkongdo <kkongdoes@naver.com>
2024-06-05 19:16:28 +0900

    #47 UI: Facilities UI

---
+++ src/components/Filter/Facilities.jsx
@@ -0,0 +1,12 @@
+const Facilites = () => {
+  return (
+    <div className='p-4 border-b-2 border-solid border-gray-200'>
+      <div className='pb-6'>
+        <h2 className='text-md font-semibold pb-2'>
+          <span>편의시설</span>
+        </h2>
+      </div>
+    </div>
+  )
+}
+export default Facilites
