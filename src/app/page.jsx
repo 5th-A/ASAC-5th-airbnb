b1d690e9e6722e9b20173e26557e7e7ad9449a6e
parby8605 <164837908+parby8605@users.noreply.github.com>
2024-06-21 17:20:32 +0900

    #64 refactor: fetch추가 및 구조 변경

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,13 @@
-import Category from '@/components/Category/Category'
 import Header from '@/components/Header/Header'
-import RoomList from '@/components/RoomList/RoomList'
+import MainPageWrapper from '@/components/MainPageWrapper/MainPageWrapper'
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center'>
       <div className='w-full'>
         <Header />
-        <Category />
       </div>
-      <RoomList />
+      <MainPageWrapper />
     </main>
   )
 }
---
+++ src/components/MainPageWrapper/MainPageWrapper.jsx
@@ -0,0 +1,33 @@
+'use client'
+
+import Category from '@/components/Category/Category'
+import RoomList from '@/components/RoomList/RoomList'
+import { useState, useEffect } from 'react'
+
+export default function MainPageWrapper() {
+  const [roomDetail, setRoomDetail] = useState([])
+  const [initRoom, setInitRoom] = useState([])
+
+  useEffect(() => {
+    async function fetchRoomData() {
+      try {
+        const response = await fetch('/RoomList.json') //GET
+        if (!response.ok) {
+          throw new Error(`숙소 목록을 불러오지 못했습니다. : ${response.status}`)
+        }
+        const data = await response.json()
+        setRoomDetail(data)
+        setInitRoom(data.slice(0, 6))
+      } catch (e) {
+        setError(e.message)
+      }
+    }
+    fetchRoomData()
+  }, [])
+  return (
+    <>
+      <Category />
+      <RoomList roomDetail={roomDetail} initRoom={initRoom} />
+    </>
+  )
+}
