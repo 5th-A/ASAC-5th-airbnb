02657cb6a30c44ade84c6c908241c3ce5d3beb9d
kkongdo <kkongdoes@naver.com>
2024-06-22 16:35:34 +0900

    #63 [FE] feat: use FilterModal useContext

--- src/components/Filter/FilterModal.jsx
+++ src/components/Filter/FilterModal.jsx
@@ -1,18 +1,11 @@
-import { useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import RoomType from './RoomType'
 import PriceRange from './PriceRangeModal'
 import Bedroom from './Bedroom'
-// import Facilities from './Facilities';
+import { FilterContext } from './FilterContext'
 
 const FilterModalComponent = ({ isOpen, onClose }) => {
-  const initialFilters = {
-    roomType: 'all',
-    priceRange: { min: 14000, max: 580000 + '+' },
-    bedrooms: { 침실: '상관없음', 침대: '상관없음', 욕실: '상관없음' },
-    statement: '방, 집 전체 등 원하는 숙소 유형을 검색해 보세요.',
-  }
-
-  const [filters, setFilters] = useState(initialFilters)
+  const { handleResetFilters } = useContext(FilterContext)
 
   useEffect(() => {
     const handleKeyDown = (event) => {
@@ -37,27 +30,6 @@ const FilterModalComponent = ({ isOpen, onClose }) => {
 
   if (!isOpen) return null
 
-  const handleRoomTypeChange = (type) => {
-    const statement = {
-      all: '방, 집 전체 등 원하는 숙소 유형을 검색해 보세요.',
-      room: '단독으로 사용하는 방이 있고, 공용 공간도 있는 형태입니다.',
-      house: '집 전체를 단독으로 사용합니다.',
-    }
-    setFilters({ ...filters, roomType: type, statement: statement[type] })
-  }
-
-  const handlePriceChange = (range) => {
-    setFilters({ ...filters, priceRange: range })
-  }
-
-  const handleBedroomChange = (bedrooms) => {
-    setFilters({ ...filters, bedrooms })
-  }
-
-  const handleResetFilters = () => {
-    setFilters(initialFilters)
-  }
-
   return (
     <section>
       <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'>
@@ -76,14 +48,9 @@ const FilterModalComponent = ({ isOpen, onClose }) => {
 
           {/* 모달 내용 - 스크롤 가능 영역 */}
           <div className='flex-1 overflow-y-auto mb-4'>
-            <RoomType
-              selectedButton={filters.roomType}
-              handleButtonClick={handleRoomTypeChange}
-              statement={filters.statement}
-            />
-            <PriceRange selectedRange={filters.priceRange} handleRangeChange={handlePriceChange} />
-            <Bedroom selectedOptions={filters.bedrooms} handleOptionChange={handleBedroomChange} />
-            {/* <Facilities /> */}
+            <RoomType />
+            <PriceRange />
+            <Bedroom />
           </div>
 
           {/* 모달 하단 - 버튼 영역 */}
