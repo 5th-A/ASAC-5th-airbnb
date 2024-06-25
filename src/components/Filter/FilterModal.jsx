61a7bf815c0505d3385d60bce9e7e9a8b96ccaff
parby8605 <lhj971011@gmail.com>
2024-06-25 20:23:36 +0900

    #78 refactor: 컴포넌트 구조 조정

--- src/components/Filter/FilterModal.jsx
+++ src/components/Filter/FilterModal.jsx
@@ -3,12 +3,9 @@ import RoomType from './RoomType'
 import PriceRange from './PriceRangeModal'
 import Bedroom from './Bedroom'
 import { FilterContext } from './FilterContext'
-import { useRouter, usePathname } from 'next/navigation'
 
 const FilterModalComponent = ({ isOpen, onClose }) => {
-  // const { filters, handleResetFilters, applyFilters } = useContext(FilterContext)
-  // const router = useRouter() // useRouter 훅 사용
-  // const pathname = usePathname() // 현재 경로 가져오기
+  const { filters, handleResetFilters, applyFilters } = useContext(FilterContext)
 
   useEffect(() => {
     const handleKeyDown = (event) => {
@@ -34,41 +31,14 @@ const FilterModalComponent = ({ isOpen, onClose }) => {
   const handleApplyFilters = () => {
     const filteredData = applyFilters() // 필터링된 데이터 얻기
     console.log(filteredData) // 필터링된 데이터 콘솔에 출력
-
-    // 1. URL QueryString 쓰기
-    // 2. <Link> 써서 덕용님한테 보내기.
-    // <Link href={`/s${selectedCity}?${queryString}`>
-    // </Link>
-
-    // Logic 짜야 할 순서
-    // 1. usePathname();// 현재 내 URL 판단한다. /s/가 있는지 없는지 판단해서 분기 설정(있으면 덕용님, 없으면 희정 형님 또는 내가 렌더링)
-    // 2. /s/ 있으면 URL QueryString 만들어서 내가 가지고 있는 상태값들 다 넣기
-    // 3. <Link>써서 더굥님한테 보내기, 없으면 희정형님한테 보낸다.
-
-    // // 1. URL QueryString 쓰기
-    // const queryString = new URLSearchParams({
-    //   roomType: filters.roomType,
-    //   minPrice: filters.priceRange.min,
-    //   maxPrice: filters.priceRange.max,
-    //   bedRooms: filters.bedrooms['침실'],
-    //   beds: filters.bedrooms['침대'],
-    //   bathRooms: filters.bedrooms['욕실'],
-    // }).toString()
-
-    // // 2. 현재 경로에 따라 분기 처리
-    // if (pathname.includes('/s/')) {
-    //   router.push(`/s/?${queryString}`) // /s 경로가 있으면 덕용님 페이지로 이동
-    // }
-
     onClose() // 모달 닫기
   }
-
   if (!isOpen) return null
 
   return (
     <section>
       <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'>
-        <div className='w-[600px] max-h-[90vh] border-b-8 bg-white rounded-lg flex flex-col'>
+        <div className='w-[45%] max-h-[90vh] border-b-8 bg-white rounded-lg flex flex-col'>
           {/* 모달 상단 - 필터 제목 */}
           <header className='flex justify-between items-center border-b-2 border-solid border-gray-200'>
             <div className='w-full min-h-16 flex justify-center items-center px-3 relative'>
@@ -89,7 +59,7 @@ const FilterModalComponent = ({ isOpen, onClose }) => {
           </div>
 
           {/* 모달 하단 - 버튼 영역 */}
-          <footer className='flex border-t-2 justify-between items-center border-t-2 border-solid border-gray-200 p-3'>
+          <footer className='flex justify-between items-center border-t-2 border-solid border-gray-200 p-3'>
             <button
               className='hover:bg-gray-500 focus:outline-none focus:ring focus:ring-gray-300 rounded'
               onClick={handleResetFilters}
--- src/components/MainPageWrapper/MainPageWrapper.jsx
+++ src/components/MainPageWrapper/MainPageWrapper.jsx
@@ -1,12 +1,19 @@
 'use client'
-
+import Header from '@/components/Header/Header'
 import Category from '@/components/Category/Category'
 import RoomList from '@/components/RoomList/RoomList'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import { useSelector } from 'react-redux'
+import { FilterContext } from '@/components/Filter/FilterContext'
 
 export default function MainPageWrapper() {
+  const { filters } = useContext(FilterContext)
   const [roomDetail, setRoomDetail] = useState([])
   const [initRoom, setInitRoom] = useState([])
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const { adults, teens, kids, pets } = useSelector((state) => state.guestCount)
+  const { selectedStartDate, selectedEndDate } = useSelector((state) => state.setCalendar)
 
   useEffect(() => {
     async function fetchRoomData() {
@@ -24,8 +31,27 @@ export default function MainPageWrapper() {
     }
     fetchRoomData()
   }, [])
+
+  useEffect(() => {
+    async function fetchRoomData() {
+      try {
+        setSearchQuery(``)
+        const response = await fetch('/RoomList.json') //추후 완성된 searchQuery대입한 url로 변경
+        if (!response.ok) {
+          throw new Error(`숙소 목록을 불러오지 못했습니다. : ${response.status}`)
+        }
+        const data = await response.json()
+        setRoomDetail(data)
+        setInitRoom(data.slice(0, 6))
+      } catch (e) {
+        setError(e.message)
+      }
+    }
+    fetchRoomData()
+  }, [filters])
   return (
     <>
+      <Header />
       <Category />
       <RoomList roomDetail={roomDetail} initRoom={initRoom} />
     </>
