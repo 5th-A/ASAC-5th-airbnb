8df1b16e7e50440d0ce36915271f8412f8892f9c
HeeJeong Lee <lhj971011@gmail.com>
2024-06-26 17:36:25 +0900

    #78 feat: 필터모달에서 요청시 사용할 fetch 메서드 작성

--- src/components/MainPageWrapper/MainPageWrapper.jsx
+++ src/components/MainPageWrapper/MainPageWrapper.jsx
@@ -14,6 +14,20 @@ export default function MainPageWrapper() {
 
   const { adults, teens, kids, pets } = useSelector((state) => state.guestCount)
   const { selectedStartDate, selectedEndDate } = useSelector((state) => state.setCalendar)
+  /** 필터에서 숙소 보기 할 경우 fetch하는 렌더링 메서드 */
+  async function fetchByFilter() {
+    try {
+      const response = await fetch('') //api주소 넣기
+      if (!response.ok) {
+        throw new Error(`숙소 목록을 불러오지 못했습니다. : ${response.status}`)
+      }
+      const data = await response.json()
+      setRoomDetail(data)
+      setInitRoom(data.slice(0, 6))
+    } catch (e) {
+      setError(e.message)
+    }
+  }
 
   useEffect(() => {
     async function fetchRoomData() {
@@ -33,24 +47,24 @@ export default function MainPageWrapper() {
   }, [])
 
   useEffect(() => {
-    async function fetchRoomData() {
+    async function updateQuery() {
       try {
         setSearchQuery(
           `checkin=${selectedStartDate}&checkout=${selectedEndDate}&children=${teens}&adults=${adults}&infants=${kids}&pets=${pets}&minPrice=${filters.priceRange.min}&maxPrice=${filters.priceRange.max}&bath=${filters.bedrooms.욕실}&bed=${filters.bedrooms.침대}&bedRoom=${filters.bedrooms.침실}&roomType=${filters.roomType}`,
         )
-        const response = await fetch('/RoomList.json') //추후 완성된 searchQuery대입한 api로 변경
-        if (!response.ok) {
-          throw new Error(`숙소 목록을 불러오지 못했습니다. : ${response.status}`)
-        }
-        const data = await response.json()
-        setRoomDetail(data)
-        setInitRoom(data.slice(0, 6))
+        // const response = await fetch('/RoomList.json') //추후 완성된 searchQuery대입한 api로 변경
+        // if (!response.ok) {
+        //   throw new Error(`숙소 목록을 불러오지 못했습니다. : ${response.status}`)
+        // }
+        // const data = await response.json()
+        // setRoomDetail(data)
+        // setInitRoom(data.slice(0, 6))
       } catch (e) {
         setError(e.message)
       }
     }
     fetchRoomData()
-  }, [filters])
+  }, [filters, selectedStartDate, selectedEndDate, adults, teens, kids, pets])
   return (
     <>
       <Header searchQuery={searchQuery} />
